29f2ed28fafaf1e6acae6f4220c7c6da06d36a1d
Taranovskaya <s5198653@yandex.ru>
2024-05-03 22:05:49 +0300

    add main catalog

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import "./style.css";
 import "./css/fonts.css";
 import "./css/variables.css";
 import "./css/header.css";
+import "./css/catalog.css";
 import "./css/footer.css";
 
 import "./js/main";
