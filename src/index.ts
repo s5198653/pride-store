29e25d955fc12a4c7d77953ffaab0a702e02e17e
Taranovskaya <s5198653@yandex.ru>
2024-05-04 13:16:40 +0300

    add products

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import "./css/fonts.css";
 import "./css/variables.css";
 import "./css/header.css";
 import "./css/catalog.css";
+import "./css/products.css";
 import "./css/footer.css";
 
 import "./js/main";
