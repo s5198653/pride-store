a717deba2bb19328a2257448d3d73235b6ae1302
Taranovskaya <s5198653@yandex.ru>
2024-06-27 16:22:48 +0300

    feat: adds product images

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="vitest" />
 import path from "path";
-import { defineConfig } from "vite";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+import { ViteMinifyPlugin } from "vite-plugin-minify";
 import packageJson from "./package.json";
 
 const getPackageName = () => {
@@ -23,7 +24,7 @@ const fileName = {
 
 const formats = Object.keys(fileName) as Array<keyof typeof fileName>;
 
-module.exports = defineConfig({
+export default {
   base: "./",
   build: {
     outDir: "./build/dist",
@@ -41,4 +42,56 @@ module.exports = defineConfig({
       { find: "@@", replacement: path.resolve(__dirname) },
     ],
   },
-});
+  plugins: [
+    ViteMinifyPlugin({}),
+    ViteImageOptimizer({
+      test: /\.(jpe?g|png|svg)$/i,
+      includePublic: false,
+      logStats: true,
+      ansiColors: true,
+      svg: {
+        multipass: true,
+        plugins: [
+          {
+            name: "preset-default",
+            params: {
+              overrides: {
+                cleanupNumericValues: false,
+                convertPathData: {
+                  floatPrecision: 2,
+                  forceAbsolutePath: false,
+                  utilizeAbsolute: false,
+                },
+                removeViewBox: false, // https://github.com/svg/svgo/issues/1128
+                cleanupIds: false,
+              },
+            },
+          },
+          "removeDimensions",
+        ],
+      },
+      png: {
+        // https://sharp.pixelplumbing.com/api-output#png
+        quality: 80,
+        palette: true,
+      },
+      jpeg: {
+        // https://sharp.pixelplumbing.com/api-output#jpeg
+        quality: 80,
+        progressive: true,
+      },
+      jpg: {
+        // https://sharp.pixelplumbing.com/api-output#jpeg
+        quality: 80,
+        progressive: true,
+      },
+      webp: {
+        // https://sharp.pixelplumbing.com/api-output#webp
+        lossless: true,
+      },
+      // Cache assets in cacheLocation. When enabled, reads and writes asset files with their hash suffix from the specified path.
+      cache: false,
+      cacheLocation: undefined,
+    }),
+  ],
+};
