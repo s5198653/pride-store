ede09148715bee56e259e4fec467ff9f765956f8
Taranovskaya <s5198653@yandex.ru>
2024-07-08 16:09:59 +0300

    upgrade styles

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,1 @@
-import "./style.css";
-import "./css/fonts.css";
-import "./css/variables.css";
-import "./css/modal.css";
-import "./css/title.css";
-import "./css/header.css";
-import "./css/footer.css";
-import "./css/contacts.css";
-import "./css/catalog.css";
-import "./css/products.css";
-import "./css/info.css";
-
 import "./js/main";
